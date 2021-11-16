33d011ea7a0ddd2fe278c28d4b166801d26ce00c
Beta Lin <sb2828sb@gmail.com>
2021-11-16 15:10:28 +0800

    upload model_v1

--- app.py
+++ app.py
@@ -25,13 +25,14 @@ from linebot import (
     LineBotApi, WebhookHandler
 )
 import os
-line_bot_api=LineBotApi(channel_access_token=os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
-handler=WebhookHandler(channel_secret=os.environ["LINE_CHANNEL_SECRET"])
+line_bot_api = LineBotApi(
+    channel_access_token=os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
+handler = WebhookHandler(channel_secret=os.environ["LINE_CHANNEL_SECRET"])
 
 
 # 載入Follow事件
 from linebot.models.events import (
-    FollowEvent,UnfollowEvent,MessageEvent,TextMessage,PostbackEvent,ImageMessage,AudioMessage,VideoMessage
+    FollowEvent, UnfollowEvent, MessageEvent, TextMessage, PostbackEvent, ImageMessage, AudioMessage, VideoMessage
 )
 
 
@@ -45,21 +46,25 @@ from google.cloud.logging.handlers import CloudLoggingHandler
 client = google.cloud.logging.Client()
 
 # 建立line event log，用來記錄line event
-bot_event_handler = CloudLoggingHandler(client,name="ncu_bot_event")
-bot_event_logger=logging.getLogger('ncu_bot_event')
+bot_event_handler = CloudLoggingHandler(client, name="ncu_bot_event")
+bot_event_logger = logging.getLogger('ncu_bot_event')
 bot_event_logger.setLevel(logging.INFO)
 bot_event_logger.addHandler(bot_event_handler)
 
 app = Flask(__name__)
+
 
 @app.route('/test')
 def hello_world():
     bot_event_logger.info("test")
     return 'Hello, World!'
 
+
 '''
 轉發功能列表
 '''
+
+
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -77,38 +82,47 @@ def callback():
 
     return 'OK'
 
+
 @handler.add(FollowEvent)
 def handle_line_follow(event):
     return LineBotController.follow_event(event)
+
 
 @handler.add(UnfollowEvent)
 def handle_line_unfollow(event):
     return LineBotController.unfollow_event(event)
 
-@handler.add(MessageEvent,TextMessage)
+
+@handler.add(MessageEvent, TextMessage)
 def handle_line_text(event):
     return LineBotController.handle_text_message(event)
 
-@handler.add(MessageEvent,ImageMessage)
+
+@handler.add(MessageEvent, ImageMessage)
 def handle_line_image(event):
     return LineBotController.handle_image_message(event)
 
-@handler.add(MessageEvent,VideoMessage)
+
+@handler.add(MessageEvent, VideoMessage)
 def handle_line_video(event):
     return LineBotController.handle_video_message(event)
 
-@handler.add(MessageEvent,AudioMessage)
+
+@handler.add(MessageEvent, AudioMessage)
 def handle_line_audio(event):
     return LineBotController.handle_audio_message(event)
+
 
 @handler.add(PostbackEvent)
 def handle_postback_event(event):
     return LineBotController.handle_postback_event(event)
 
-@app.route("/user",methods=['GET'])
+
+@app.route("/user", methods=['GET'])
 def get_user():
     result = UserController.get_user(request)
     return result
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
